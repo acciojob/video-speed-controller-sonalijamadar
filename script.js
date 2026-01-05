599a6f2705960e494d7bd28dd2535275124e5aa5
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2026-01-05 12:56:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const  toggle = player.querySelector('.toggle');
+const  slider = player.querySelector('.player_slider');
+const skipButtons = player.querySelector('[data-skip]');
+
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]()
+}
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚ ';
+}
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+sliders.forEach(slider =>
+  slider.addEventListener('input', handleRangeUpdate)
+);
+skipButtons.forEach(button =>
+  button.addEventListener('click', skip)
+);
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
